deed73d9f06796e927e54b14458296e36dedc0b2
gaganjot99 <gagankahal00@gmail.com>
2022-07-23 16:56:23 +0530

    fix: cors added

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const cors = require("cors");
 const {dbStart} = require("./db/db");
 const path = require("path")
 const {startApollo, typeDefs, resolvers} = require("./graphql/index");
 
 const app = express();
+app.use(cors())
 app.use(express.static(path.join(__dirname + "/build")))
 app.get("/",(req, res)=>{
   res.sendFile(path.join(__dirname, "./build/index.html"))
